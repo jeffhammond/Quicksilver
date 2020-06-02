3023ee82c307b936160cc576d5062f7bf3e7b3b6
Jeff Hammond <jeff.r.hammond@intel.com>
2020-06-02 10:40:50 -0700

    remove unnecessary DPCT union name

--- src/dpct/mpi_stubs_internal.hh
+++ src/dpct/mpi_stubs_internal.hh
@@ -66,7 +66,7 @@ typedef struct _Handleitem
   int handle;
   struct _Handleitem *next;
 
-   union dpct_type_d8b7ed
+   union type_d8b7ed
   {
     void *anything;           // At least size of void *
     Comm comm;
