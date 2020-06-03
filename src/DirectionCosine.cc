55ce3067c548ceff065b329085f2c39c3c757f3e
Jeff Hammond <jeff.r.hammond@intel.com>
2020-06-03 09:18:45 -0700

    use math utility header

--- src/DirectionCosine.cc
+++ src/DirectionCosine.cc
@@ -1,20 +1,7 @@
 #include "DirectionCosine.hh"
 #include "MC_RNG_State.hh"
 #include "PhysicalConstants.hh"
-
-#ifdef HAVE_SYCL
-#include <CL/sycl.hpp>
-#define SIN  sycl::sin
-#define COS  sycl::cos
-#define SQRT sycl::sqrt
-#define LOG  sycl::log
-#else
-#include <cmath>
-#define SIN  sin
-#define COS  cos
-#define SQRT sqrt
-#define LOG  log
-#endif
+#include "mathHelp.hh"
 
 void DirectionCosine::Sample_Isotropic(uint64_t *seed)
 {
